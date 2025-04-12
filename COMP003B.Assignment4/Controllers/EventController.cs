b68767f8ecd7751f6c66b9fade47b2babe6e5b56
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-04-11 20:06:45 -0700

    Added an Event Controller in the file "EventController.cs." Added code that sets up the tournament registration form in the file "EventRegistration.cs." Set up the layout of the cards for the form in the file "Register.cshtml." Added a message telling the user that they have successfully applied for tournament registration in the file "Success.cshtml." Adjusted the event layout in the file "_EventNav.cshtml." Added a tag to register for the fighting game tournament of choice in the file "_Layout.cshtml."

---
+++ COMP003B.Assignment4/Controllers/EventController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.Assignment4.Models;
+namespace COMP003B.Assignment4.Controllers
+{
+	public class EventController : Controller
+	{
+		public IActionResult Register()
+		{
+			return View(new EventRegistration());
+		}
+		[HttpPost]
+		public IActionResult Register(EventRegistration model)
+		{
+			if (ModelState.IsValid)
+			{
+				return View("Success", model);
+			}
+			return View(model);
+		}
+	}
+}
---
+++ COMP003B.Assignment4/Models/EventRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.Assignment4.Models
+{
+	public class EventRegistration
+	{
+		[Required]
+		[Display(Name = "Full Name")]
+		public string FullName { get; set; }
+		[Required]
+		[EmailAddress]
+		[Display(Name = "Email Address")]
+		public string Email { get; set; }
+		[Required]
+		[Display(Name = "Event Code")]
+		public string EventCode { get; set; }
+		[Required]
+		[Range(1, 10)]
+		[Display(Name = "Number of Tickets")]
+		public int Tickets { get; set; }
+		[Display(Name = "Referral Code (Optional)")]
+		public string ReferralCode { get; set; }
+	}
+}
